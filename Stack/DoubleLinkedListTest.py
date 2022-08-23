e5dc4b8a316fd98ec335b27775e99800b551a9b0
T-Lind <tiernanxklindauer@gmail.com>
2022-08-23 13:59:24 -0500

    DoubleLinkedList.py works mostly, still need to finish push_front

---
+++ Stack/DoubleLinkedListTest.py
@@ -0,0 +1,14 @@
+from DoubleLinkedList import DoubleLinkedList
+
+my_list = DoubleLinkedList(0)
+
+my_list.push(1)
+my_list.push(2)
+my_list.push(3)
+
+
+print(my_list.pop())
+print(my_list.pop())
+print(my_list.pop_front())
+print(my_list.pop_front())
+
