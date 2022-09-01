928529ce46fc5378305d2ec38e347526b2633934
T-Lind <tiernanxklindauer@gmail.com>
2022-09-01 16:38:11 -0500

    eight to one selector!

--- VirtualComputer/LogicGates.py
+++ VirtualComputer/LogicGates.py
@@ -4,8 +4,18 @@ def _and(a, b):
     return a & b
 
 
+def _quad_and(a, b, c, d):
+    return _and(_and(a, b), _and(c, d))
+
+
 def _or(a, b):
     return a | b
+
+
+def _eight_or(a, b, c, d, e, f, g, h):
+    first_four_or = _or(_or(a, b), _or(c, d))
+    second_four_or = _or(_or(e, f), _or(g, h))
+    return _or(first_four_or, second_four_or)
 
 
 def _not(a):
@@ -86,6 +96,9 @@ class _r_s_flip_flop:
         self.q = False
         self.q_bar = False
 
+    def get_state(self):
+        return self.q
+
     def __call__(self, r, s):
         q_bar = self.q_bar
         q = self.q
@@ -99,8 +112,12 @@ class _d_type_flip_flop:
     def __init__(self):
         self.flip_flop = _r_s_flip_flop()
 
-    def __call__(self, clk, data):
-        return self.flip_flop(_and(_not(data), clk), _and(data, clk))
+    def __call__(self, write, data):
+        return self.flip_flop(_and(_not(data), write), _and(data, write))
+
+    def get_state(self):
+        return self.flip_flop.get_state()
+
 
 class _8_bit_latch:
     def __init__(self):
@@ -113,3 +130,88 @@ class _8_bit_latch:
         self.latch_6 = _d_type_flip_flop()
         self.latch_7 = _d_type_flip_flop()
 
+    def __call__(self, d_7, d_6, d_5, d_4, d_3, d_2, d_1, d_0, write):
+        return self.latch_7(write, d_7)[0], self.latch_6(write, d_6)[0], self.latch_5(write, d_5)[0], \
+               self.latch_4(write, d_4)[0], \
+               self.latch_3(write, d_3)[0], self.latch_2(write, d_2)[0], self.latch_1(write, d_1)[0], \
+               self.latch_0(write, d_0)[0]
+
+    def get_state(self):
+        return self.latch_7.get_state(), self.latch_6.get_state(), self.latch_5.get_state(), self.latch_4.get_state(), \
+               self.latch_3.get_state(), self.latch_2.get_state(), self.latch_1.get_state(), self.latch_0.get_state()
+
+
+class _2_to_1_line_selector:
+    def _single(self, a, b, select):
+        one = _and(a, _not(select))
+        two = _and(b, select)
+        return _or(one, two)
+
+    def __call__(self, a_7, a_6, a_5, a_4, a_3, a_2, a_1, a_0,
+                 b_7, b_6, b_5, b_4, b_3, b_2, b_1, b_0, select
+                 ):
+        return self._single(a_7, b_7, select), self._single(a_6, b_6, select), self._single(a_5, b_5, select), \
+               self._single(a_4, b_4, select), self._single(a_3, b_3, select), self._single(a_2, b_2, select), \
+               self._single(a_1, b_1, select), self._single(a_0, b_0, select)
+
+
+class _edge_d_flip_flop:
+    def __init__(self):
+        self.q = False
+        self.q_bar = False
+
+        self.q_mid = False
+        self.q_mid_bar = False
+
+    def __call__(self, data, clk):
+        def stage_1(clock=clk):
+            clock = _not(clock)
+
+            and_1 = _and(data, clock)
+            and_2 = _and(_not(data), clock)
+
+            nor_1 = _nor(and_1, self.q_mid_bar)
+            nor_2 = _nor(and_2, self.q_mid)
+
+            self.q_mid = nor_1
+            self.q_mid_bar = nor_2
+
+        def stage_2(clock=clk):
+            and_1 = _and(self.q_mid, clock)
+            and_2 = _and(self.q_mid_bar, clock)
+
+            nor_1 = _nor(and_1, self.q_bar)
+            nor_2 = _nor(and_2, self.q)
+
+            self.q = nor_1
+            self.q_bar = nor_2
+
+        # Run the first half of the flip flop
+        stage_1()
+
+        # Run the second half of the flip flop
+        stage_2()
+
+        return self.q, self.q_bar
+
+    def get(self):
+        return self.q
+
+
+def _8_to_1_line_selector(s_0, s_1, s_2,
+                          d_0, d_1, d_2, d_3, d_4, d_5, d_6, d_7
+                          ):
+    inv_s_0 = _not(s_0)
+    inv_s_1 = _not(s_1)
+    inv_s_2 = _not(s_2)
+
+    and_0 = _quad_and(d_0, inv_s_0, inv_s_1, inv_s_2)
+    and_1 = _quad_and(d_1, s_0, inv_s_1, inv_s_2)
+    and_2 = _quad_and(d_2, inv_s_0, s_1, inv_s_2)
+    and_3 = _quad_and(d_3, s_0, s_1, inv_s_2)
+    and_4 = _quad_and(d_4, inv_s_0, inv_s_1, s_2)
+    and_5 = _quad_and(d_5, s_0, inv_s_1, s_2)
+    and_6 = _quad_and(d_6, inv_s_0, s_1, s_2)
+    and_7 = _quad_and(d_7, s_0, s_1, s_2)
+
+    return _eight_or(and_0, and_1, and_2, and_3, and_4, and_5, and_6, and_7)
--- VirtualComputer/VirtualComputerInterface.py
+++ VirtualComputer/VirtualComputerInterface.py
@@ -1,7 +1,11 @@
-from VirtualComputer.LogicGates import _d_type_flip_flop
+from VirtualComputer.LogicGates import _edge_d_flip_flop
 
 # print(_8_bit_adder(True, False, False, False, True, False, True, True,
 #                    False, True, False, True, False, True, False, False,
 #                    subtract=False
 #                          ))
 
+ff = _edge_d_flip_flop()
+
+print(ff(1, 0))
+print(ff.get())
