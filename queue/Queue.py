ba6091ac6324c4af7eb64bcd20bbc88cb44358a1
T-Lind <tiernanxklindauer@gmail.com>
2022-09-30 13:30:48 -0500

    queues

---
+++ queue/Queue.py
@@ -0,0 +1,28 @@
+class Queue:
+    def __init__(self):
+        """Create new deque"""
+        self._items = []
+
+    def is_empty(self):
+        """Check if the deque is empty"""
+        return not bool(self._items)
+
+    def add_front(self, item):
+        """Add an item to the front of the deque"""
+        self._items.append(item)
+
+    def add_rear(self, item):
+        """Add an item to the rear of the deque"""
+        self._items.insert(0, item)
+
+    def remove_front(self):
+        """Remove an item from the front of the deque"""
+        return self._items.pop()
+
+    def remove_rear(self):
+        """Remove an item from the rear of the deque"""
+        return self._items.pop(0)
+
+    def size(self):
+        """Get the number of items in the deque"""
+        return len(self._items)
