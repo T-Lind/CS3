09466846702b75b04858a564278d4770c953c7a2
T-Lind <tiernanxklindauer@gmail.com>
2022-10-31 12:39:35 -0500

    hanoi graphics work

--- hanoi/TowerOfHanoi.py
+++ hanoi/TowerOfHanoi.py
@@ -6,7 +6,7 @@ pygame.init()
 window = pygame.display.set_mode((576, 576))
 image = pygame.image.load(r'temple.jpg')
 
-solver = TowersOfHanoi(10, pygame, window, height=20, action_delay=0)
+solver = TowersOfHanoi(4, pygame, window, height=20, action_delay=0.1)
 solver()
 
 input("Press 'Enter' to exit")
