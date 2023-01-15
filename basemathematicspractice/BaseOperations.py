aef18a01ebd3e51ae8d0d9795158baa49c515a54
T-Lind <tiernanxklindauer@gmail.com>
2023-01-15 12:12:16 -0600

    custom cs3 binary/hex adding program

---
+++ basemathematicspractice/BaseOperations.py
@@ -0,0 +1,74 @@
+hex_to_dec_table = {"0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "A": 10, "B": 11,
+                    "C": 12, "D": 13, "E": 14, "F": 15}
+
+def bin_to_hex(number: str, hex_nums=128) -> str:
+    bin_number = int(number)
+    temp = 0
+    mult = 1
+
+    cnt = 1
+
+    hexNum = ["0"] * hex_nums
+
+    i = 0
+    while bin_number > 0:
+        remainder = bin_number % 10
+        temp = temp + (remainder * mult)
+
+        if cnt % 4 == 0:
+            if temp < 10:
+                hexNum[i] = chr(temp + 48)
+            else:
+                hexNum[i] = chr(temp + 55)
+
+            mult = 1
+            temp = 0
+            cnt = 1
+            i += 1
+
+        else:
+            mult *= 2
+            cnt = cnt + 1
+        bin_number = int(bin_number / 10)
+
+    if cnt != 1:
+        hexNum[i] = chr(temp + 48)
+
+    if cnt == 1:
+        i -= 1
+
+    ret_str = ""
+
+    while i >= 0:
+        ret_str += hexNum[i]
+        i -= 1
+    return ret_str
+
+def hex_to_dec(hex_number: str) -> str:
+    result: int = 0
+    size = len(hex_number) - 1
+
+    for character in hex_number:
+        result = result + hex_to_dec_table[character] * 16 ** size
+        size -= 1
+
+    return str(result)
+
+def add_bin(num1: str, num2: str) -> str:
+    result = ''
+    carry = 0
+
+    for i in range(max(len(num1), len(num2)) - 1, -1, -1):
+        remainder = carry
+        remainder += 1 if int(num1[i]) else 0
+        remainder += 1 if int(num2[i]) else 0
+        result = ('1' if remainder % 2 == 1 else '0') + result
+
+        carry = 0 if remainder <= 1 else 1
+
+    if carry != 0:
+        result = '1' + result
+
+    return result.zfill(max(len(num1), len(num2)))
+
+
---
+++ basemathematicspractice/ComputeSheet.py
@@ -0,0 +1,18 @@
+from BaseOperations import bin_to_hex, hex_to_dec, add_bin
+
+def add_bin_nums(num1, num2):
+    max_len = max(len(num1), len(num2))
+    num1.zfill(max_len-len(num1))
+    num2.zfill(max_len-len(num2))
+    result = add_bin(num1, num2)
+    hex_result = bin_to_hex(result)
+    print(f" {num1}\n+{num2}\n{'-'*(max_len+1)}\n{result}(2) or {hex_result}(16) or {hex_to_dec(hex_result)}(10)\n")
+
+if __name__ == '__main__':
+    print("ADDING BINARY NUMBERS WORKSHEET\n--------------")
+    print("Solving row 2 and 5:\n")
+
+    add_bin_nums("101101", "110010")
+    add_bin_nums("101010", "110100")
+    add_bin_nums("101001", "110101")
+    add_bin_nums("100110", "110110")
