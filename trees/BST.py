b460f2f16a66502746754362e5f35f0179f64ecd
T-Lind <tiernanxklindauer@gmail.com>
2023-04-03 13:09:53 -0500

    BST finished

--- trees/BST.py
+++ trees/BST.py
@@ -48,7 +48,7 @@ class BinarySearchTree:
                 queue.append((node.left, level + 1))
             if node.right:
                 queue.append((node.right, level + 1))
-        return max(cur_count, max_count)
+        return max(cur_count, max_count) + 1
 
     def get_height(self):
         return self._get_height(self.root)
@@ -72,6 +72,7 @@ class BinarySearchTree:
 
     def print_in_order(self):
         self._print_in_order(self.root)
+        print()
 
     def _print_in_order(self, current_node):
         if current_node:
@@ -79,8 +80,19 @@ class BinarySearchTree:
             print(current_node.data, end=" ")
             self._print_in_order(current_node.right)
 
+    def print_in_reverse(self):
+        self._print_in_reverse(self.root)
+        print()
+
+    def _print_in_reverse(self, current_node):
+        if current_node:
+            self._print_in_reverse(current_node.right)
+            print(current_node.data, end=" ")
+            self._print_in_reverse(current_node.left)
+
     def print_pre_order(self):
         self._print_pre_order(self.root)
+        print()
 
     def _print_pre_order(self, current_node):
         if current_node:
@@ -90,9 +102,54 @@ class BinarySearchTree:
 
     def print_post_order(self):
         self._print_post_order(self.root)
+        print()
 
     def _print_post_order(self, current_node):
         if current_node:
             self._print_post_order(current_node.left)
             self._print_post_order(current_node.right)
             print(current_node.data, end=" ")
+
+    def _get_num_nodes(self, node):
+        if node is None:
+            return 0
+        return 1 + self._get_num_nodes(node.left) + self._get_num_nodes(node.right)
+
+    def get_num_nodes(self):
+        return self._get_num_nodes(self.root)
+
+    def _get_num_levels(self, node):
+        if node is None:
+            return 0
+        else:
+            return 1 + max(self._get_num_levels(node.left), self._get_num_levels(node.right))
+
+    def get_num_levels(self):
+        return self._get_num_levels(self.root)
+
+    def _is_full(self, node):
+        if node is None:
+            return True
+        elif node.left is None and node.right is None:
+            return True
+        elif node.left is not None and node.right is not None:
+            return self._is_full(node.left) and self._is_full(node.right)
+        return False
+
+    def is_full(self):
+        return self._is_full(self.root)
+
+    def _str(self, node):
+        ret_str = ""
+        if node:
+            ret_str += self._str(node.left) + f"{node.data} " + self._str(node.right)
+        return ret_str
+
+    def str(self):
+        return self._str(self.root)
+
+    def __str__(self):
+        ret_str = ""
+        for value in self._str(self.root):
+            ret_str += value
+        return ret_str
--- trees/Run-BST.py
+++ trees/Run-BST.py
@@ -9,4 +9,22 @@ tree.insert(100)
 tree.insert(98)
 tree.insert(120)
 
+print("In order:")
 tree.print_in_order()
+
+print("Pre order:")
+tree.print_pre_order()
+
+print("Post order:")
+tree.print_post_order()
+
+print("Reverse order:")
+tree.print_in_reverse()
+
+print(f"Height: {tree.get_height()}")
+print(f"Width: {tree.get_width()}")
+print(f"# of leaves: {tree.get_num_leaves()}")
+print(f"# of nodes: {tree.get_num_nodes()}")
+print(f"# of levels: {tree.get_num_levels()}")
+print(f"Tree as string: {tree}")
+print(f"Tree is full: {tree.is_full()}")
